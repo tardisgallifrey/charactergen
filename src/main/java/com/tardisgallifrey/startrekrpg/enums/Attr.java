11e600db80fd76ddc835082b7c8cec5b34ba8928
tardisgallifrey <bigmanrunning@gmail.com>
2023-12-24 11:35:58 -0600

    Hashmap fix 12/23/23

--- src/main/java/com/tardisgallifrey/startrekrpg/enums/Attr.java
+++ src/main/java/com/tardisgallifrey/startrekrpg/enums/Attr.java
@@ -1,27 +1,11 @@
 package com.tardisgallifrey.startrekrpg.enums;
 
 public enum Attr {
-    CONTROL("Control", 1),
-    DARING("Daring", 1),
-    FITNESS("Fitness", 1),
-    INSIGHT("Insight", 1),
-    PRESENCE("Presence", 1),
-    REASON("Reason", 1);
+    CONTROL,
+    DARING,
+    FITNESS,
+    INSIGHT,
+    PRESENCE,
+    REASON
 
-    public String getLabel() {
-        return label;
-    }
-
-    private final String label;
-
-    public int getAdder() {
-        return adder;
-    }
-
-    private final int adder;
-
-    Attr(String label, int i) {
-        this.label = label;
-        this.adder = i;
-    }
 }
